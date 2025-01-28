81e7c935a3cfe8dfff231e96e163d103e441caa6
Shubham Saurav <ssaurav28502@gmail.com>
2025-01-28 14:33:32 +0530

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Calculator{
+    public static void Main(){
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("1 for Add, 2 for Subtract");
+        int choice = int.Parse(Console.ReadLine());
+        switch (choice){
+            case 1:
+                Console.WriteLine(Add(a, b));
+                break;
+            case 2:
+                Console.WriteLine(Sub(a, b));
+                break;
+            default:
+                Console.WriteLine("Invalid choice!");
+                break;
+        }
+    }
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Sub(int a, int b)
+    {
+        return a - b;
+    }
+}
